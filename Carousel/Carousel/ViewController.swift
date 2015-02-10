7ca4755c1ece4140f55958be89049df8de23662c
Brian Bailey <brian@bailey.is>
2015-02-10 10:21:35 -0800

    Build Xcode project

---
+++ Carousel/Carousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carousel
+//
+//  Created by Brian Bailey on 2/10/15.
+//  Copyright (c) 2015 i had that dream again. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
